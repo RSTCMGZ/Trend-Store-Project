9e52ebfe8354d445211a27114f33bc624265579e
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-11-13 14:18:18 +0300

    off-canvas-open-close

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import Header from "./components/layout/Header";
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+  const hideCartHandler = (e) => {
+    e.preventDefault();
+    setCartIsShow(false);
+  };
   return (
     <div className="App">
-      <Cart />
-      <Header />
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <Hero />
       <Products />
     </div>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable react/prop-types */
 import "./Cart.css";
-const Cart = () => {
+import CartItem from "./CartItem";
+import products from "../../productData";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className="cart-items">
+      {products.map((product) => (
+        <CartItem key={product.id} product={product} />
+      ))}
+    </ul>
+  );
   return (
     <div className="offcanvas">
       <div className="content">
         <div className="cart-head">
           <h2>Sepetim</h2>
-          <a href="#">X</a>
+          <a href="#" className="cart-close" onClick={props.onClose}>
+            X
+          </a>
         </div>
-        <div className="cartItems"></div>
+        {cartItems}
         <div className="total">
           <span>Toplam Değer</span>
           <span>10₺</span>
